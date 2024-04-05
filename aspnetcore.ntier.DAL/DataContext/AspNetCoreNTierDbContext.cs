de04d1972eb94f857224fc0b134a704903c03a53
AndriiZlt <117361082+AndriiZlt@users.noreply.github.com>
2024-04-05 17:30:16 +0300

    register/login

---
+++ aspnetcore.ntier.DAL/DataContext/AspNetCoreNTierDbContext.cs
@@ -0,0 +1,26 @@
+﻿using aspnetcore.ntier.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnetcore.ntier.DAL.DataContext;
+
+public class AspNetCoreNTierDbContext : DbContext
+{
+    public AspNetCoreNTierDbContext(DbContextOptions<AspNetCoreNTierDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasData(
+             new User
+             {
+                 UserId = 1,
+                 Username = "johndoe",
+                 Email = "johndoe@gmail.com",
+                 Password = "123",
+                 Name = "John",
+                 Surname = "Doe",
+             }
+         );
+    }
+}
---
+++ aspnetcore.ntier.DTO/DTOs/UserToUpdateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace aspnetcore.ntier.DTO.DTOs;
+
+public class UserToUpdateDTO
+{
+    public int UserId { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+}
